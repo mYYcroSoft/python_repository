e6dbed97eb08591ad84b63d839021e235a4f7dfb
mYcroSoft <j.sivco22@gmail.com>
2023-10-15 15:24:44 +0200

    Encryptor

--- encryotorDecryptor/index.py
+++ encryotorDecryptor/index.py
@@ -1,5 +1,5 @@
 
-
+"""
 class rail:
     def __init__(self, string_world = 0):
         self.stringWorlds = string_world
@@ -43,3 +43,41 @@ encrypt = encrypt(string)
 encrypt.genereateRailsArray()
 
 print(encrypt.writeRail())
+
+"""
+
+
+class encryptRail:
+    string = ''
+    number_of_rails = 0
+    rails = []
+    math = [0, 1, 2, 1, 0, 1, 2, 1, 0,1,2, 1, 2, 0, 1,2]
+
+    def __init__(self, string , number_of_rails = 3):
+         self.__string = string.replace(" ", "")
+         self.__number_of_rails = number_of_rails
+         self.rails = [[] for _ in range(self.__number_of_rails)]
+         
+    def encrypt(self):
+        string_list = list(self.__string)
+        for x in range(len(self.__string)):
+            rail_index = self.math[x]
+            self.rails[rail_index].append(string_list[x])
+        return self.rails
+
+        
+    def __str__(self):
+        return f'Původní string: {self.__string} O 3 kolejnicích s výstupem: {self.rails}'
+
+
+string = "AHOJ JAK JE"
+
+encrypt_text = encryptRail(string)
+encrypt_text.encrypt()
+print(encrypt_text)
+
+
+
+    
+
+    
---
+++ encryotorDecryptor/test.py
@@ -0,0 +1,15 @@
+string = "AHOJJAKJE"
+math = [0, 1, 2, 1, 0, 1, 2, 1, 0,1,2, 1, 2, 0, 1,2]
+rails = [[], [], []]
+string_list = list(string)
+
+
+
+for x in range(len(string)):
+    print (f'x: {x}  math: {math[x]} > {string_list[x]}')
+    print( math[x])
+    rail_index = math[x]
+    rails[rail_index].append(string_list[x])
+
+print(rails)
+
