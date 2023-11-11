9bb388eb62cb02b78d5770ba89be0173c0e59e14
mYcroSoft <j.sivco22@gmail.com>
2023-11-11 18:13:03 +0100

    data

--- py_problem_solve/index.py
+++ py_problem_solve/index.py
@@ -1,28 +1,40 @@
 import random
 
+
+class brake:
+    def __init__(self, first_brake_quality):
+        self.__first_brake_quality = first_brake_quality
+        self.generate_max_brake()
+    def generate_max_brake(self):
+         self.brake_quality = random.randint(1, self.__first_brake_quality + 1)
+    def getBrake(self):
+        return self.brake_quality
+
+class EngineSpeed:
+    def __init__(self, first_max_speed):
+        self.__first_max_speed = first_max_speed
+        self.generate_max_speed()
+    def generate_max_speed(self):
+         speed_quant = self.__first_max_speed / 2
+         self.max_speed = random.randint(speed_quant + (random.randint(0,6)) - random.randint(0,3) , speed_quant * random.randint(2,4) - random.randint(0,7))
+    def getEnginespeed(self):
+        return self.max_speed
+    
+
 class enemy:
-    def __init__(self, first_vehicle_name, first_max_speed, first_brake_quality):
-        self.__first_vehicle_name = first_vehicle_name
+    def __init__(self, vehicle_name, first_max_speed, first_brake_quality):
         self.__first_max_speed = first_max_speed
         self.__first_brake_quality = first_brake_quality
+        self.__vehicle_name = vehicle_name
         self.generate_enemy()
 
     def generate_enemy(self):
-        self.__vehicle_name = "Tojotinka"  
-        self.__max_speed = self.generate_max_speed()
-        self.__brake_quality = self.generate_brake_quality()
-
-    def generate_max_speed(self):
-         speed_quant = self.__first_max_speed / 2
-         max_speed = random.randint(speed_quant + (random.randint(0,6)) - random.randint(0,3) , speed_quant * random.randint(2,4) - random.randint(0,7))
-         return max_speed
-
-    def generate_brake_quality(self):
-        brake_quality = random.randint(1, self.__first_brake_quality + 1)
-        return brake_quality
+        self.Engine =  brake(self.__first_max_speed)
+        self.Brakes = EngineSpeed(self.__first_max_speed)
 
     def __str__(self):
-        print(f"Soupořovo vozidlo {self.__vehicle_name} má maximální rychlost {self.__max_speed} a kvalitu brzd {self.__brake_quality}")
+        engine = self.Engine
+        return f"Vozidlo soupeře: {self.__vehicle_name} má maximální rychlost {engine.getEnginespeed()} a kvalitu brzd {self.__brakes.getBrake()}"
     pass
 
 class vehicle:
