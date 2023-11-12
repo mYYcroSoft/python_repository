d434245288f44c9ba3f114fd3300533c842caca9
mYcroSoft <j.sivco22@gmail.com>
2023-11-12 12:18:38 +0100

    Problem solve

--- py_problem_solve/index.py
+++ py_problem_solve/index.py
@@ -1,6 +1,5 @@
 import random
 
-
 class brake:
     def __init__(self, first_brake_quality):
         self.__first_brake_quality = first_brake_quality
@@ -26,30 +25,60 @@ class enemy:
         self.__first_max_speed = first_max_speed
         self.__first_brake_quality = first_brake_quality
         self.__vehicle_name = vehicle_name
-        self.generate_enemy()
+         # ---------------------------------------------------------------------------------------
+        self.Brakes =  brake(self.__first_max_speed)
+        self.Engine = EngineSpeed(self.__first_max_speed)
 
-    def generate_enemy(self):
-        self.Engine =  brake(self.__first_max_speed)
-        self.Brakes = EngineSpeed(self.__first_max_speed)
-
+    def getEnemyData(self):
+        return {"Name": self.__vehicle_name, "Engine": self.Engine.getEnginespeed(), "Brake": self.Brakes.getBrake()}
+    
+    
     def __str__(self):
-        engine = self.Engine
-        return f"Vozidlo soupeře: {self.__vehicle_name} má maximální rychlost {engine.getEnginespeed()} a kvalitu brzd {self.__brakes.getBrake()}"
+     
+        return f"Vozidlo soupeře: {self.__vehicle_name} má maximální rychlost {self.Engine.getEnginespeed()} a kvalitu brzd {self.Brakes.getBrake()}"
     pass
 
 class vehicle:
     def __init__(self, vehicle_name, max_speed, brake_quality):
         self.__vehicle_name = vehicle_name
-        self.__max_speed = max_speed
-        self.__brake_quality = brake_quality
+        self.__default_max_speed = max_speed
+        self.__default_max_brake_quality = brake_quality
+        # ---------------------------------------------------------------------------------------
+        self.Engine = EngineSpeed(self.__default_max_speed)
+        self.Brakes = brake(self.__default_max_brake_quality)        
+        self.enemy = enemy("Skoda", self.__default_max_speed, self.__default_max_brake_quality) 
 
+    def GetEnemy(self):
+        enemy = self.enemy.getEnemyData()
+        return f"Vozidlo soupeře {enemy['Name']} má maximální rychlost {enemy['Engine']} a kvalitu brzd  {enemy['Brake']}"
+    
+    def getEnemyData(self):
+        return self.enemy.getEnemyData()
+    
+    def vehicleData(self):
+        return {"Name": self.__vehicle_name, "Engine": self.Engine.getEnginespeed(), "Brake": self.Brakes.getBrake()} 
+        
+    def __str__(self):
+        return  f"Tvoje vozidlo: {self.__vehicle_name} má maximální rychlost {self.Engine.getEnginespeed()} a kvalitu brzd {self.Brakes.getBrake()}" 
+        
+    
     pass
 
 
 
-car_enemy = enemy('Škodovka', 20, 2)
-
-print(car_enemy)
 
 
+#car_enemy = enemy('Škodovka', 20, 2)
+#print(car_enemy)
+
+vehicle_name = str(input("Název vozidla: "))
+max_speed = int(input("(max 40) Maximální rychlost: "))
+max_brake = int(input("(max 40) Maximální kvalita brzd: "))
+
+
+vehicles = vehicle("Škoda", 20, 2)
+print("__________________________________________")
+print(vehicles)
+print(vehicles.GetEnemy())
+print("__________________________________________")
 
