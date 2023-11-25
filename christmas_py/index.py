c064a1e2c92d176b64cbfd65f94f82536490b788
mYcroSoft <j.sivco22@gmail.com>
2023-11-25 17:29:04 +0100

    christmas

--- christmas_py/index.py
+++ christmas_py/index.py
@@ -2,77 +2,70 @@ import pygame
 import random
 import math
 
+class Snowfall:
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+    def __init__(self):
+        pygame.init()
+        self.snowflakes = []
 
-x = 100
-y = 0
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.screen = pygame.display.set_mode((1280, 720))
 
+    def start(self):
+        while self.running:
+            self.screen.fill((0, 0, 0))  
+            self.background = pygame.image.load("back.jpg")
+            self.screen.blit(self.background, (0, 0))
 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.snowflakes.append(
+                Snowflake(random.randrange(-40, 1320), random.randrange(-50, -10), 4, (150, 150, 150), 2.3,self.screen, self.snowflakes))
+
+            self.snowflakes.append(
+                Snowflake(random.randrange(-40, 1320), random.randrange(-50, -10), 3, (255, 255, 255), 2, self.screen,self.snowflakes))
+
+            for snowflake in self.snowflakes:
+                snowflake.calculate()
+                snowflake.draw()
+
+            pygame.display.flip()
+            self.clock.tick(60)
+
+        pygame.quit()
 
 class Snowflake:
-    def __init__(self, x,y, size, color, speed):
+    def __init__(self, x, y, size, color, speed, screen, snowflakes):
         self.x = x
         self.y = y
         self.speed = speed
         self.size = size
         self.color = color
-        self.toggle = False 
-        self.switch = random.randrange(0,250)
+        self.screen = screen  #
+        self.snowflakes = snowflakes
+        self.toggle = False
+        self.switch = random.randrange(0, 250)
 
     def draw(self):
         if self.y > 720:
-            del self
+            self.snowflakes.remove(self)  
         else:
-            pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
+            pygame.draw.circle(self.screen, self.color, (round(self.x), round(self.y)), self.size)
 
     def calculate(self):
         if self.y == self.switch:
             self.toggle = not self.toggle
-            self.switch = self.y + random.randrange(0,250)
-        
+            self.switch = self.y + random.randrange(0, 250)
+
         if self.toggle:
-            self.x = self.x + (math.cos(self.y/ 180) * 0.2) 
+            self.x = self.x + (math.cos(self.y / 180) * 0.2)
         else:
-            self.x = self.x - (math.sin(self.y/ 180) * 0.2)     
-        
+            self.x = self.x - (math.sin(self.y / 180) * 0.2)
+
         self.y = self.y + self.speed
 
-snowfalkes = [
-
-]
-
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
-
-
-    snowfalkes.append(Snowflake(random.randrange(-40,1320), random.randrange(-50,-10), 4, (150, 150, 150), 2.3))
-
-    snowfalkes.append(Snowflake(random.randrange(-40,1320), random.randrange(-50,-10), 3, (255, 255, 255), 2))
-
-    
-    #pygame.draw.circle(screen, (255, 255, 255), (x, y), 5)
-    for snowflake in snowfalkes:
-        snowflake.calculate()
-        snowflake.draw()
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+snowfall = Snowfall()
+snowfall.start()
