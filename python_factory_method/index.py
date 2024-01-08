da716eae9ed84bb554b31fe4fdabe773f9375d79
mYcroSoft <j.sivco22@gmail.com>
2024-01-08 10:26:38 +0100

    python factory

--- python_factory_method/index.py
+++ python_factory_method/index.py
@@ -1,32 +1,49 @@
 
-
-class computer:
+class Computer:
     def __init__(self, **kwargs):
         self.__name = kwargs['name']
         self.__ram = kwargs['ram']
         self.__cpu = kwargs['cpu']
 
+class Monitor:
+    def __init__(self, **kwargs):
+        self.__name = kwargs['name']
+        self.__display = kwargs['display']
+        self.__resolution = kwargs['resolution']
+
+class Keyboard:
+    def __init__(self, **kwargs):
+        self.__name = kwargs['name']
+        self.__type = kwargs['type']
+        self.__color = kwargs['color']
+
+class Mouse:
+    def __init__(self, **kwargs):
+        self.__name = kwargs['name']
+        self.__type = kwargs['type']
+        self.__color = kwargs['color']
 
 
-
+produkty = ['Computer', 'Monitor', 'Keyboard', 'Mouse']
 class ElectronicFactory:
-    produkty = ['Computer', 'Monitor', 'Keyboard', 'Mouse']
     def __init__(self, name):
         self.__name = name    
 
 
     @staticmethod
     def produce(**kwargs):
-        if kwargs['type'] == ElectronicFactory.produkty:
-            return globals()[kwargs['type']](**kwargs)
+        if kwargs['type'] in produkty:
+            return  globals()[kwargs['type']](**kwargs)
         else:
             return None 
 
 
 storage = []
 
-storage.append(ElectronicFactory.produce(type = 'Computer'), name ="Asus Tuf", ram = "16GB", cpu = "Intel i5")
+storage.append(ElectronicFactory.produce(type = 'Computer', name ="Asus Tuf", ram = "16GB", cpu = "Intel i5"))
 
+
+print(storage)
 
 for x in storage:
     print(x)
